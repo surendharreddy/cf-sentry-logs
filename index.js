284a28ceaac2076434b55f234c81f97fd74f674e
surendharreddy <surendharreddy.me@gmail.com>
2022-03-03 23:26:39 +0800

    add Sentry with Toucan.js

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+import Toucan from "toucan-js";
+
+const sentryDsn = process.env.SENTRY_DSN;
+let sentry;
+
 addEventListener("fetch", event => {
+  sentry = new Toucan({
+    dsn: sentryDsn,
+    context: event,
+    allowedHeaders: ["user-agent"],
+    allowedSearchParams: /(.*)/,
+    debug: false,
+    rewriteFrames: {
+      root: "/",
+    },
+    environment: process.env.ENVIRONMENT,
+    release: process.env.SENTRY_RELEASE,
+  });
+
   event.respondWith(handleRequest(event.request));
 });
+
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
+  sentry.captureMessage(`Hi, Sentry!`);
   return new Response(`Hello from ${process.env.ENVIRONMENT}!`, {
     headers: { "content-type": "text/plain" },
   });
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 const { EnvironmentPlugin } = require("webpack");
+const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 
 module.exports = {
   target: "webworker",
   entry: "./index.js",
-  plugins: [new EnvironmentPlugin(["ENVIRONMENT"])],
+  devtool: "hidden-source-map",
+  plugins: [
+    new EnvironmentPlugin(["ENVIRONMENT", "SENTRY_DSN", "SENTRY_RELEASE"]),
+    new SentryWebpackPlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      release: process.env.SENTRY_RELEASE,
+      include: "./dist",
+      urlPrefix: "/",
+    }),
+  ],
 };
